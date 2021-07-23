24efea8f5e19f69ee5884f9cd02697127455914e
Amanda <33467922+AIduate@users.noreply.github.com>
2021-07-23 00:19:06 -0400

    Update wrangle_data.py
    fixed bug in lastupdateddate from wrangle_data.py

--- wrangle_data.py
+++ wrangle_data.py
@@ -36,6 +36,14 @@ def cleandata(key_path):
     df = pd.DataFrame(lst, columns = ['last_updated_date','state','fips','cases',\
                                       'county', 'vaccines_completed_ratio', 'population', \
                                       'new_cases', 'vaccines_completed'])
+    
+    df = pd.DataFrame(lst, columns = ['last_updated_date','state','fips','cases', 'county', 'vaccines_completed_ratio', 'population','new_cases', 'vaccines_completed'])
+    df['population'] = df['population'].map('{:,.0f}'.format)
+    #df['cases'] = df['cases'].map('{:,.0f}'.format)
+    #df['new_cases'] = df['new_cases'].map('{:,.0f}'.format)
+    #df['vaccines_completed'] = df['vaccines_completed'].map('{:,.0f}'.format)
+    df['vaccines_completed_ratio'] = df['vaccines_completed_ratio'] * 100.00  
+    
     return df
 
 def return_figures():
@@ -55,13 +63,13 @@ def return_figures():
     
     fig_one = px.choropleth_mapbox(df, geojson=counties, locations='fips', color='vaccines_completed_ratio',
                                color_continuous_scale="Viridis",
-                               range_color=(0, 1),
+                               range_color=(0, 100),
                                mapbox_style="carto-positron",
                                hover_name="county",
                                hover_data=["state", "population"],
                                zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
                                opacity=0.5,
-                               labels={'vaccines_completed_ratio':'Fully Vaccinated Rate'})
+                               labels={'vaccines_completed_ratio':'fully vaccinated rate'})
     fig_two = px.choropleth_mapbox(df, geojson=counties, locations='fips', color='new_cases',
                            color_continuous_scale="temps",
                            range_color= (0, max(df['new_cases'])),
@@ -70,7 +78,7 @@ def return_figures():
                            hover_data=["state", "population"],
                            zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
                            opacity=0.5,
-                           labels={'new_cases':'New COVID-19 Cases'}
+                           labels={'new_cases':'new COVID-19 cases'}
                           )
     fig_three = px.choropleth_mapbox(df, geojson=counties, locations='fips', color='cases',
                            color_continuous_scale="reds",
@@ -80,7 +88,7 @@ def return_figures():
                            hover_data=["state", "population"],
                            zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
                            opacity=0.5,
-                           labels={'cases':'Cumulative COVID-19 cases'}
+                           labels={'cases':'cumulative COVID-19 cases'}
                           )
                       
     figures = []
@@ -91,5 +99,17 @@ def return_figures():
     return figures
     
 def return_lastupdated():
+    
+        """Pulls last updated date from api
+
+    Args:
+        None
+
+    Returns:
+        string: last updated date
+
+    """
+    
+    df = cleandata('data/config.txt')
     lastupdated = max(df['last_updated_date'])
     return lastupdated
