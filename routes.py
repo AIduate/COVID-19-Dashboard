fddd27c81c14125c660b15351e44555fdd9e08d0
Amanda <33467922+AIduate@users.noreply.github.com>
2021-07-23 00:20:42 -0400

    Update routes.py
    updated routes.py to bring in lastupdateddate

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from myapp import app
 import json, plotly
 from flask import render_template
-from wrangling_scripts.wrangle_data import return_figures
+from wrangling_scripts.wrangle_data import return_figures, return_lastupdated
 
 @app.route('/')
 @app.route('/index')
@@ -15,7 +15,10 @@ def index():
 
     # Convert the plotly figures to JSON for javascript in html template
     figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    last_updated_date = return_lastupdated()
 
     return render_template('index.html',
                            ids=ids,
-                           figuresJSON=figuresJSON)
+                           figuresJSON=figuresJSON,
+                          last_updated_date=last_updated_date)
